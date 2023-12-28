f9344a597dd4569cd7225288abff5b142a3b7206
DYes <93127741+DanielYeste@users.noreply.github.com>
2023-12-28 02:43:53 +0100

    Update target_vulnerable_ports.sh

--- target_vulnerable_ports.sh
+++ target_vulnerable_ports.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Given an Ip, proceeds to first do a sync scan with nmap and after saving the ouput in a txt file it scans looking for vulns and exports it to a file
+# Given an IP, performs an initial sync scan with nmap, saves the output in a text file, and then scans for vulnerabilities, exporting the results to a file.
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     echo "Please provide an IP address as an argument."
@@ -11,10 +11,18 @@ fi
 target_ip="$1"
 
 # Launch initial nmap scan
-nmap -p- -sS -Pn --min-rate 5000 "$target_ip" | grep -E "^[0-9]+/tcp\s+open" | cut -d'/' -f 1 > non_unknown_ports.txt
+nmap_output=$(nmap -p- -sS -Pn --min-rate 5000 "$target_ip")
+echo "$nmap_output" | grep -E "^[0-9]+/tcp\s+open" | cut -d'/' -f 1 > non_unknown_ports.txt
 
 # Create a comma-separated list of non-unknown ports
-targeted_ports=$(cat non_unknown_ports.txt | tr '\n' ',' | sed 's/,$//')
+targeted_ports=$(tr '\n' ',' < non_unknown_ports.txt | sed 's/,$//')
+
+# Check if ports are found
+if [ -z "$targeted_ports" ]; then
+    echo "No open ports found. Exiting."
+    rm non_unknown_ports.txt
+    exit 1
+fi
 
 # Launch targeted nmap scan
 sudo nmap -p "$targeted_ports" -sCV -Pn --min-rate 5000 "$target_ip" > targeted.txt
